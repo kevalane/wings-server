0f6a5778926c30f9e052492234271ff3b3cd3669
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2021-08-17 17:12:16 +0200

    Added shell of cancel autogiro function.

--- controllers/cancelController.js
+++ controllers/cancelController.js
@@ -3,6 +3,9 @@ const request = require('request');
 const axios = require('axios');
 const mongoose = require('mongoose');
 
+// Validators
+const cancelAutogiroController = require('../validators/cancel/cancelAutogiroValidator');
+
 // Global vars
 var configUrl = 'https://apitest.billecta.com';
 var creditor_id = process.env.CREDITOR_ID;
@@ -19,7 +22,31 @@ const cancel_cancelAutogiro = (req, res) => {
 		email: req.body.email
 	}
 
-	
+	const validation = cancelAutogiroValidator.validate(rawData);
+
+	if (validation.error) {
+		return res.status(400).send({err: validation.error.details[0].message});
+	} else {
+		// Successful validation, let's get the id
+		User.findOne({email: validation.value.email}).then(user => {
+			console.log(user);
+			request({
+				uri: configUrl + '/v1/contractinvoice/pause/' + user['public_id'],
+				method: 'PUT',
+				headers: headers
+			}, (err, response, body) => {
+				if (err) {
+					return res.status(400).send({err: err.message});
+				} else {
+					var result = JSON.parse(body);
+					console.log(result);
+				}
+			});
+		}).catch(err => {
+			return res.status(400).send({err: err.message});
+		});
+	}
+
 }
 
 module.exports = {
